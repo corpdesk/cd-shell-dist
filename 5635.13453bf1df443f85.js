73dc09336292d745dfa74584d5b8bcbcea820f89
“georemo” <george.oremo@gmail.com>
2024-08-04 08:36:52 +0300

    Update compiled files from cd-shell-ng-18

---
+++ 5635.13453bf1df443f85.js
@@ -0,0 +1,1 @@
+(self.webpackChunkcd_shell=self.webpackChunkcd_shell||[]).push([[5635],{5635:(f,t,e)=>{e.r(t),e.d(t,{TranslateHttpLoader:()=>h});class h{http;prefix;suffix;constructor(s,i="/assets/i18n/",a=".json"){this.http=s,this.prefix=i,this.suffix=a}getTranslation(s){return this.http.get(`${this.prefix}${s}${this.suffix}`)}}}}]);
--- 5635.d9b174dad73c1f3d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkcd_shell=self.webpackChunkcd_shell||[]).push([[5635],{85635:(f,t,e)=>{e.r(t),e.d(t,{TranslateHttpLoader:()=>h});class h{http;prefix;suffix;constructor(s,i="/assets/i18n/",a=".json"){this.http=s,this.prefix=i,this.suffix=a}getTranslation(s){return this.http.get(`${this.prefix}${s}${this.suffix}`)}}}}]);
